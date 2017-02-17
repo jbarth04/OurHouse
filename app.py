9448f2a1bcfd4e27782afaabafeaa4676c0f1891
Rachael Robinson <Rachael@Rachaels-MacBook-Pro.local>
2017-02-17 18:55:28 -0500

    added a new route to a houses page and tested out for loops/mapping in react

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-    return render_template('react_index.html')
+    return render_template('index.html')
+
+@app.route("/houses")
+def houses():
+	return render_template('houses.html')
 
 if __name__ == "__main__":
     app.run()
