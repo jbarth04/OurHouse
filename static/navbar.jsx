83ae5d5cc2433ce89871a7ca6f9ff4cd65d1b7e0
Rachael Robinson <Rachael@Rachaels-MacBook-Pro.local>
2017-02-17 19:21:55 -0500

    added a navbar file and connected it

---
+++ static/navbar.jsx
@@ -0,0 +1,6 @@
+var NavBar = React.createClass({
+        render: function() {
+            return (<p1>NAVBAR</p1>);
+        }
+    });
+React.render(<NavBar />, document.getElementById('navbar'));
