633fd65c285d890e9700a912b961f5e5c7a58f8d
Rachael Robinson <Rachael@Rachaels-MacBook-Pro.local>
2017-02-28 15:52:19 -0500

    hard coding houses to work on filters

---
+++ static/houses.js
@@ -0,0 +1,11 @@
+houses = [1, 2, 3, 4, 5, 6, 10, 15, 23, 28, 34, 37];
+// {
+// 	"id":1,
+// 	"addrNum": 54,
+// 	"street": "sunset rd",
+// 	"city":"somerville",
+// 	"zipcode": 02144,
+// 	"bedrooms": 4,
+// 	"parking": true,
+// 	"rent": 750
+// }
