dcfb9136026d7ce0908b744763479535f8647432
Josie Barth <josie.barth@gmail.com>
2017-03-01 12:17:41 -0500

    trying to get database to work

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template
-
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
+# # get database info from secret config.py file, NOT pushed to github or heroku
+# app.config.from_pyfile('config.py')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# # make sure we got the database URL
+# if app.config['SQLALCHEMY_DATABASE_URI'] == None:
+#     print "Need database config"
+#     sys.exit(1)
+
+# db = SQLAlchemy(app)
+
+###################
+from sqlalchemy import create_engine, MetaData, Table
+
+engine = create_engine('postgresql:///josie_test', convert_unicode=True)
+metadata = MetaData(bind=engine)
+
+students = Table('OurHouse.Students', metadata, autoload=True)
+###################
 
 @app.route("/")
-
 def index():
     return render_template('index.html')
 
